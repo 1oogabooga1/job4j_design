9e72b3a01f6b8e4aa962ac63df0e6b5cc6275dc2
1oogabooga1 <dmitrijdobrydin@gmail.com>
2023-12-18 22:02:02 +0300

    7. ListIterator [#350217 #467327]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -7,26 +7,14 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.nextIndex() == index) {
-                iterator.add(value);
-                break;
-            }
-            iterator.next();
-        }
+        ListIterator<T> iterator = list.listIterator(index);
+        iterator.add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.previousIndex() == index) {
-                iterator.add(value);
-                break;
-            }
-            iterator.next();
-        }
+        ListIterator<T> iterator = list.listIterator(index + 1);
+        iterator.add(value);
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
@@ -49,14 +37,6 @@ public class ListUtils {
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        ListIterator<T> first = list.listIterator();
-        for (T currentElement : elements) {
-            while (first.hasNext()) {
-                if (currentElement.equals(first.next())) {
-                    first.remove();
-                }
-            }
-            first = list.listIterator();
-        }
+        list.removeIf(elements::contains);
     }
 }
