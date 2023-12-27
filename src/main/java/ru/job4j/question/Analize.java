9469fb86259e34c1ac10842bc79f62f3075585b1
1oogabooga1 <dmitrijdobrydin@gmail.com>
2023-12-27 23:27:10 +0300

    3. Статистика по коллекции. [#45889 #468707]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -7,32 +7,23 @@ public class Analize {
     public static Info diff(Set<User> previous, Set<User> current) {
         Info rsl = new Info(0, 0, 0);
         Map<Integer, User> prev = new HashMap<>();
-        previous.forEach(k -> prev.put(k.getId(), k));
-        Map<Integer, User> curr = new HashMap<>();
-        current.forEach(k -> curr.put(k.getId(), k));
-        Iterator<User> first = prev.values().iterator();
-        Iterator<User> second = curr.values().iterator();
-        while (first.hasNext() && second.hasNext()) {
-            User firstUser = first.next();
-            User secondUser = second.next();
-            if (prev.size() < curr.size()) {
-                rsl.setAdded(+1);
-            }
-            if (firstUser.getId() != secondUser.getId()
-                    && !firstUser.getName().equals(secondUser.getName())
-                    && previous.size() <= current.size()) {
-                rsl.setAdded(+1);
-            }
-            if (firstUser.getId() == secondUser.getId()
-                    && !firstUser.getName().equals(secondUser.getName())) {
-                rsl.setChanged(+1);
-            }
-            if (firstUser.getId() != secondUser.getId()
-                    && !firstUser.getName().equals(secondUser.getName())
-                    && previous.size() >= current.size()) {
-                rsl.setDeleted(+1);
+        previous.forEach(user -> prev.put(user.getId(), user));
+        int changed = 0;
+        int added = 0;
+        for (User currentUser : current) {
+            User prevUser = prev.get(currentUser.getId());
+            if (prevUser != null) {
+                if (!prevUser.equals(currentUser)) {
+                    changed++;
+                }
+                prev.remove(currentUser.getId());
+            } else {
+                added++;
             }
         }
+        rsl.setDeleted(prev.size());
+        rsl.setAdded(added);
+        rsl.setChanged(changed);
         return rsl;
     }
 }
