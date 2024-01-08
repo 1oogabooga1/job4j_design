f1cdc7071943fbad99836a6d5f9c35a1fed3fdae
1oogabooga1 <dmitrijdobrydin@gmail.com>
2024-01-08 14:40:04 +0300

    2.1. FileInputStream [#4898 #471131]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream input = new FileInputStream("data/even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = input.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] numbers = text.toString().split(System.lineSeparator());
+            StringBuilder rsl = new StringBuilder();
+            for (String s : numbers) {
+                int number = Integer.parseInt(s);
+                if (number % 2 == 0) {
+                    rsl.append(number);
+                    rsl.append(System.lineSeparator());
+                }
+            }
+            System.out.println(rsl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
