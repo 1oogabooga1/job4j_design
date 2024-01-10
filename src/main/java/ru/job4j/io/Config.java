018b8b3fdbada12e860c0e7ffbd14ccb4fc55db7
1oogabooga1 <dmitrijdobrydin@gmail.com>
2024-01-10 05:50:57 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,56 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader input = new BufferedReader(new FileReader(path))) {
+            for (String line = input.readLine(); line != null; line = input.readLine()) {
+                String[] lines = line.split("=", 2);
+                if (lines.length < 2
+                        || lines[0].equals(" ")
+                        || lines[1].equals(" ")
+                ) {
+                    throw new IllegalArgumentException();
+                } else if (!line.contains("#")) {
+                    values.put(lines[0], lines[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+       return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner output = new StringJoiner(System.lineSeparator());
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines().forEach(output::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenPairWithCommentIsNull() {
+        String path = "data/pair_with_comment.properties";
+        Config con = new Config(path);
+        con.load();
+        assertThat(con.value("thirdName")).isNull();
+    }
+
+    @Test
+    void whenPairWithCommentHasValue() {
+        String path = "data/pair_with_comment.properties";
+        Config con = new Config(path);
+        con.load();
+        assertThat(con.value("secondName")).isEqualTo("Vasya Vasenkov");
+    }
+
+    @Test
+    void whenWithoutKeyThenThrowsIllegalArgumentException() {
+        String path = "data/pair_without_key.properties";
+        Config con = new Config(path);
+        assertThrows(IllegalArgumentException.class,
+                con::load);
+    }
+
+    @Test
+    void whenWithoutValueThenThrowsIllegalArgumentException() {
+        String path = "data/pair_without_value.properties";
+        Config con = new Config(path);
+        assertThrows(IllegalArgumentException.class,
+                con::load);
+    }
+
+    @Test
+    void whenWithoutEquallyThenThrowsIllegalArgumentException() {
+        String path = "data/pair_without_equally.properties";
+        Config con = new Config(path);
+        assertThrows(IllegalArgumentException.class,
+                con::load);
+    }
+
+    @Test
+    void whenWithoutValueAndKeyThenThrowsIllegalArgumentException() {
+        String path = "data/without_value_and_key.properties";
+        Config con = new Config(path);
+        assertThrows(IllegalArgumentException.class,
+                con::load);
+    }
+
+    @Test
+    void whenOnlyEquallyThenThrowsIllegalArgumentException() {
+        String path = "data/pair_with_only_equally.properties";
+        Config con = new Config(path);
+        assertThrows(IllegalArgumentException.class,
+                con::load);
+    }
+
+    @Test
+    void whenValueWithEquallyComment() {
+        String path = "./data/pair_with_value_with_equally.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr=");
+    }
+}
