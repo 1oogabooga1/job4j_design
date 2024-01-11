30194c5ccd3196aa474d86511d75445159cd2198
1oogabooga1 <dmitrijdobrydin@gmail.com>
2024-01-12 00:31:17 +0300

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader input = new BufferedReader(new FileReader(source));
+             PrintWriter output = new PrintWriter(
+                     new BufferedOutputStream(
+                     new FileOutputStream(target)
+                ))) {
+            boolean work = true;
+            for (String line = input.readLine(); line != null; line = input.readLine()) {
+                String[] lines = line.split(" ");
+                if (work && ("400".equals(lines[0]) || "500".equals(lines[0]))) {
+                    work = false;
+                    output.println(lines[1]);
+                }
+                if (!work && (!"400".equals(lines[0]) && !"500".equals(lines[0]))){
+                    work = true;
+                    output.println(lines[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
