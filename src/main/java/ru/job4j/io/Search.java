c1c8e1f1d12483ae123b9a967bcd6e8d53e61daf
1oogabooga1 <dmitrijdobrydin@gmail.com>
2024-01-17 04:52:42 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,22 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (validate(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static boolean validate(String[] args) {
+        if (args.length == 0 || args[1].isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        return true;
     }
 }
