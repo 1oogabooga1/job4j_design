b7ff18d847a4f49d9c82a2c136b9f92b99098d52
1oogabooga1 <dmitrijdobrydin@gmail.com>
2024-01-24 17:52:10 +0300

    14. Архивировать проект [#861 #475896]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -31,7 +31,7 @@ public class SearchFiles implements FileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (condition.test(file)) {
-            System.out.println(file.toAbsolutePath());
+            paths.add(file);
         }
         return FileVisitResult.CONTINUE;
     }
--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,9 +1,9 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -13,8 +13,7 @@ public class Zip {
     public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (Path path : sources) {
-                Path relativePath = Paths.get("").relativize(target.toPath());
-                zip.putNextEntry(new ZipEntry(relativePath.toString()));
+                zip.putNextEntry(new ZipEntry(path.toAbsolutePath().toString()));
                 try (BufferedInputStream output = new BufferedInputStream(new FileInputStream(path.toFile()))) {
                     zip.write(output.readAllBytes());
                 }
@@ -37,26 +36,34 @@ public class Zip {
     }
 
     private static boolean validate(String[] args) {
+        ArgsName keyValue = ArgsName.of(args);
+        if (keyValue.get("d").isBlank() || keyValue.get("e").isBlank() || keyValue.get("o").isBlank()) {
+            throw new IllegalArgumentException("Missing some argument");
+        }
+        if (!Files.isDirectory(Paths.get(keyValue.get("d")))) {
+            throw new IllegalArgumentException("Invalid path");
+        }
+        if (!keyValue.get("e").startsWith(".") || keyValue.get("e").length() <= 1) {
+            throw new IllegalArgumentException("Invalid file extension");
+        }
+        if (!keyValue.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException("Incorrect archive extension");
+        }
         return true;
     }
 
-    public static void main(String[] args) {
-        Zip zip = new Zip();
-        String sourceDirectory = ArgsName.of(args).get("d");
-        String targetZipFile = ArgsName.of(args).get("o");
-        Path sourcePath = Paths.get(sourceDirectory);
-        List<Path> sourceFiles = new ArrayList<>();
-        File sourceDir = new File(sourceDirectory);
-        File[] files = sourceDir.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                sourceFiles.add(file.toPath());
-            }
+    public static void main(String[] args) throws IOException {
+        if (validate(args)) {
+            Zip zip = new Zip();
+            String sourceDirectory = ArgsName.of(args).get("d");
+            String targetZipFile = ArgsName.of(args).get("o");
+            Path sourcePath = Paths.get(sourceDirectory);
+            List<Path> sourceFiles = Search.search(sourcePath, path -> true);
+            zip.packFiles(sourceFiles, new File(targetZipFile));
+            zip.packSingleFile(
+                    new File("./pom.xml"),
+                    new File("./pom.zip")
+            );
         }
-        zip.packFiles(sourceFiles, new File(targetZipFile));
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
     }
 }
