fa9666277efd9a9eb578f88b281984ef2799a4c3
Dmitrii <dmitrijdobrydin@gmail.com>
2024-10-04 19:41:35 +0300

    Aлгоритм двух указателей [#505153]

---
+++ src/main/java/ru/job4j/leetCode/SmallestRangeFinder.java
@@ -0,0 +1,38 @@
+package ru.job4j.leetCode;
+
+
+import java.util.*;
+
+public class SmallestRangeFinder {
+    public static int[] findSmallestRange(int[] nums, int k) {
+        int left = 0;
+        int right = left + 1;
+        int[] rsl = new int[2];
+        List<Integer> result = new ArrayList<>();
+        while (left < nums.length) {
+            if (right == nums.length) {
+                if (result.size() + 1 == k) {
+                    result.add(left);
+                    rsl[0] = result.get(0);
+                    rsl[1] = result.get(result.size() - 1);
+                    return rsl;
+                } else {
+                    return null;
+                }
+            }
+            if (nums[left] != nums[right]) {
+                result.add(left);
+            } else {
+                result.clear();
+            }
+            if (result.size() == k) {
+                rsl[0] = result.get(0);
+                rsl[1] = result.get(result.size() - 1);
+                return rsl;
+            }
+            left++;
+            right++;
+        }
+        return null;
+    }
+}
