df1223b624430f772e927249e1d1b58ad45be9b5
Dmitrii <dmitrijdobrydin@gmail.com>
2024-11-20 10:19:24 +0300

    3.1. Добавить изменения в последний коммит. [#504829]

---
+++ src/main/java/ru/job4j/leetcode/Water.java
@@ -0,0 +1,34 @@
+package ru.job4j.leetcode;
+
+class Water {
+    public static int maxArea(Integer[] height) {
+        int firstPointer = 0;
+        int secondPointer = height.length - 1;
+        int maxArea = 0;
+        int width;
+        int currentHeight;
+        int area;
+        int prevArea = 0;
+        while (firstPointer < secondPointer) {
+            width = secondPointer - firstPointer;
+            currentHeight = Math.min(height[firstPointer],
+                    height[secondPointer]);
+            area = currentHeight * width;
+            maxArea = Math.max(prevArea, area);
+            if (area > prevArea) {
+                prevArea = area;
+            }
+            if (height[firstPointer] < height[secondPointer]) {
+                firstPointer++;
+            } else {
+                secondPointer--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        Integer[] height = new Integer[] {1, 8, 6, 2, 5, 4, 8, 3, 7, 8};
+        System.out.println(maxArea(height));
+    }
+}
